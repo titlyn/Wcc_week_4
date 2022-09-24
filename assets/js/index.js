10b4662b0cc06509a6df3ed1b5b0b1386cda1540
titlyn <nyaina163@gmail.com>
2022-09-24 21:43:43 +0300

    svg

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,7 @@ function myFunction () {
 window.onclick = function (event) {
     if (!event.target.matches('.menu')) {
     var dropdowns = document.getElementsByClassName("list");
-    var openDropdown = dropdowns[i];
+    var openDropdown;
         if (openDropdown.classList.contains('show')) {
             openDropdown.classList.remove('show');
         }
