c51134ab872a9c3c13667468dc31a1bdd846ac40
Tolotra <tolotramandresy39@gmail.com>
2022-09-25 19:20:02 +0300

    tablette version on the way to be done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,9 @@
-function myFunction () {
-    document.getElementById("list").classList.toggle("show");
-    console.log("click")
-}
-
-window.onclick = function (event) {
-    if (!event.target.matches('.menu')) {
-    var dropdowns = document.getElementsByClassName("list");
-    var openDropdown;
-        if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-        }
-    }
-}
-
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
